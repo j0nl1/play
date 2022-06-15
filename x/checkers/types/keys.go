e945339d3617994489705ed84bba290171269185
j0nl1 <jonlitech@gmail.com>
2022-06-15 23:51:46 +0200

    feat: deadline param in stored game and increase deadline when moving

--- x/checkers/types/keys.go
+++ x/checkers/types/keys.go
@@ -1,5 +1,7 @@
 package types
 
+import "time"
+
 const (
 	// ModuleName defines the module name
 	ModuleName = "checkers"
@@ -35,6 +37,10 @@ const (
 	RejectGameEventKey     = "GameRejected"
 	RejectGameEventCreator = "Creator"
 	RejectGameEventIdValue = "IdValue"
+
+	// Deadline keys
+	MaxTurnDuration = time.Duration(1 * 3_600 * 1000_000_000) // 1 hour
+	DeadlineLayout  = "2006-01-02 15:04:05.999999999 +0000 UTC"
 )
 
 func KeyPrefix(p string) []byte {
